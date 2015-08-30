114aa042b3a411b7b7b50108309c965f91a3f6e8
ashleybennett <adb2016@uga.edu>
2015-08-30 15:01:50 -0400

    Minus

--- project_1/src/Table.java
+++ project_1/src/Table.java
@@ -230,7 +230,6 @@ public class Table
 
         List <Comparable []> rows = new ArrayList <> ();
 
-        //  T O   B E   I M P L E M E N T E D 
         //  Implemented by Ashley Bennett
 
         for (Comparable[] tuple : tuples) {
